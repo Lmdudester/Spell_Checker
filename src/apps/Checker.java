4b5a4603e5aa01f3ef66ef1edbccb5bb374081dc
Liam Davies <lmdudester@gmail.com>
2017-05-01 19:29:30 -0400

    Added dictionary contruction

--- src/apps/Checker.java
+++ src/apps/Checker.java
@@ -1,25 +1,67 @@
 package apps;
 
 import structures.*;
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Checker {
 
+	private static Dictionary makeDictionary(String fName, FileReader fr){
+		Dictionary lang = new Dictionary(fName);
+		BufferedReader bufRdr = new BufferedReader(fr);
+		StringTokenizer tknzr = null;
+		String tknstrm = null;
+		
+		try{ 
+			tknstrm = bufRdr.readLine();
+			
+		} catch(IOException e){
+			System.out.println("Error: IOException on BufferedReader");
+			return null;
+		}
+		
+		while(tknstrm != null){
+			tknzr = new StringTokenizer(tknstrm);
+			
+			while(tknzr.hasMoreTokens()){
+				lang.addWord(tknzr.nextToken());
+			}
+			
+			try{
+				tknstrm = bufRdr.readLine();
+				
+			}catch(IOException e){
+				System.out.println("Error: IOException on BufferedReader");
+				return null;
+			}
+		}
+		
+		return lang;
+	}
+	
 	public static void main(String[] args) {
-		Dictionary eng = new Dictionary("English");	
+		Scanner input = new Scanner(System.in);
+		FileReader fr = null;
+		String fName;
 		
-		eng.addWord("0do");
-		eng.addWord("1do");
-		eng.addWord("2do");
-		eng.addWord("3do");
-		eng.addWord("4do");
-		eng.addWord("5do");
-		eng.addWord("6do");
-		eng.addWord("7do");
-		eng.addWord("8do");
-		eng.addWord("9do");
-		eng.addWord("ado");
+		do{
+			System.out.print("Please enter a Language: ");
+			fName = input.nextLine();
+			
+			try{
+				fr = new FileReader(fName.toLowerCase() + ".txt");
+			} catch(FileNotFoundException e){
+				fr = null;
+				System.out.println("File not found, please try again.");
+			}
+		}while(fr == null);
 		
-		System.out.print("List: " + eng.toString());
+		input.close();
+		
+		Dictionary lang = makeDictionary(fName, fr);
+		
+		System.out.println("English: "+ lang.toString());
 		
 	}
 
